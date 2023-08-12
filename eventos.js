c9309246b10d03447698a5ac9ffecd9354975c64
camilaprincipe <141370399+camilaprincipe@users.noreply.github.com>
2023-08-12 16:05:55 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btn");
+
+    btn.addEventListener("click", () => {
+        alert("Hola! Soy el div");
+    });
+});
+
+
+
+
+
+
+
